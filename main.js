d147fc30fc9b9d300439ddccedede1de317c6a69
Timmehthepro <125130270+Timmehthepro@users.noreply.github.com>
2024-05-11 11:29:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function open_roblox(){
+    window.open("https://www.roblox.com/NewLogin")
+}
+
+function open_fortnite(){
+    window.open("https://www.fortnite.com/")
+}
+
+function open_TSB(){
+    window.open("https://www.roblox.com/games/10449761463/The-Strongest-Battlegrounds")
+}
+function open_bloxfruits(){
+    window.open("https://www.roblox.com/games/7449423635/Blox-Fruits-Third-Sea")
+}
+function open_jjk(){
+    window.open("https://www.roblox.com/games/9391468976/Jujutsu-Shenanigans-RESTLESS-GAMBLER")
+}
+function open_gd(){
+    window.open("https://www.crazygames.com/game/geometry-dash-online")
+}
